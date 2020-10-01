b093cf90bb60fa4af6b385b01425047bb75c9bbb
Saurabh Pujari <saurabhpuj99@gmail.com>
2020-10-02 00:56:20 +0530

    update done with gitmeup

--- bin/index.js
+++ bin/index.js
@@ -13,7 +13,7 @@ inquirer
   .prompt([
     {
       name: 'branchname',
-      message: 'Enter branch name (default : main) : ',
+      message: 'Enter branch name (main) : ',
       default: 'main',
     },
     {
@@ -28,8 +28,41 @@ inquirer
     commit_message = answers.commitMessage;
     console.log(branch_name);
     console.log(commit_message);
+
+    git.add('.')
+    .then(
+       (addSuccess) => {
+          //console.log(addSuccess);
+          console.log(chalk.green('\nAll files added successfully'))
+          git.commit(commit_message)
+            .then(
+               (success) => {
+                //console.log(successCommit);
+                  console.log(chalk.magenta('Commit successful : ') + chalk.yellowBright(commit_message))
+                  git.pull('origin',branch_name)
+                     .then((success) => {
+                        console.log(chalk.cyan('Pull successful'));
+                        git.push('origin',branch_name)
+                           .then((success) => {
+                              console.log(chalk.blue('Changes pushed successfully'));
+                              process.exit();
+                           },(failed)=> {
+                              console.log(chalk.red('Push failed'));
+                           });
+                     },(failed)=> {
+                        console.log(chalk.red('Pull failed'));
+                  });
+               }, (failed) => {
+                  console.log(chalk.red('Commit failed'));
+         });
+       }, (failedAdd) => {
+          console.log(chalk.red('Adding files failed'));
+    });
+
+
   });
 
+  /*
   // Add all files for commit
   git.add('.')
     .then(
